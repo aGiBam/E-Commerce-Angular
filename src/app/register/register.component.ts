6777377a150d8d07b1ed910163f0bfcecc912418
aGiBam <hamo.hamada61@gmail.com>
2025-02-02 09:45:44 +0200

    Register Form Validation Updated

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,33 +7,37 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  registerForm: FormGroup = new FormGroup({
-    name: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(8),
-    ]),
-    email: new FormControl(null, [
-      Validators.required,
-      Validators.email,
-      Validators.minLength(5),
-      Validators.maxLength(20),
-    ]),
-    password: new FormControl(null, [
-      Validators.required,
-      Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
-    ]),
-    rePassword: new FormControl(null, [
-      Validators.required,
-      Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
-    ]),
-    phone: new FormControl(null, [
-      Validators.required,
-      Validators.pattern(/^(01)[125][0-9]{8}$/),
-    ]),
-  });
+  registerForm: FormGroup = new FormGroup(
+    {
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(8),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
+      ]),
+      rePassword: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
+      ]),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^(01)[125][0-9]{8}$/),
+      ]),
+    },
+    this.validatePassword
+  );
 
   registerSubmit(rForm: FormGroup) {
     console.log(rForm);
+  }
+
+  validatePassword(g: any) {
+    return g.get('password').value === g.get('rePassword').value
+      ? null
+      : { notSame: true };
   }
 }
