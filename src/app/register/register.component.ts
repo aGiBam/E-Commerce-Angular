e2b5bd0ed4c616b5d58b3fa97938d51380f911ec
Mohammed Ahmed Agiba <hamo.hamada61@gmail.com>
2025-02-18 16:10:57 +0200

    Duplicated FormGroup Cleared

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -35,31 +35,6 @@ export class RegisterComponent {
     this.validatePassword
   );
 
-  registerForm: FormGroup = new FormGroup(
-    {
-      name: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(8),
-      ]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
-      ]),
-      rePassword: new FormControl(null, [
-        Validators.required,
-        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
-      ]),
-      phone: new FormControl(null, [
-        Validators.required,
-        Validators.pattern(/^(01)[125][0-9]{8}$/),
-      ]),
-    },
-    this.validatePassword
-  );
-
-
   registerSubmit(rForm: FormGroup) {
     this._AuthService.sendRegister(rForm.value).subscribe({
       next: (res) => {
